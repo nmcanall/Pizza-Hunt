77796cbb923e9da812be18d43c8f444ff8e4e862
nmcanall <nmcanall@citadel.edu>
2020-09-16 23:39:32 -0700

    comment routes

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -5,6 +5,15 @@ const pizzaController = {
     // Get all pizzas at api/pizzas
     getAllPizza(req, res) {
         Pizza.find({})
+            // populate allows you to see data from comment model rather than just comment id
+            .populate({
+                path: "comments",
+                select: "-__v"
+            })
+            // Tell Pizza model not to display default __v property
+            .select("-__v")
+            // Sort the pizzas in descending order (newest first)
+            .sort({_id: -1})
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.log(err);
@@ -15,6 +24,11 @@ const pizzaController = {
     // Get one pizza at api/pizzas/id
     getPizzaById({params}, res) {
         Pizza.findOne({_id: params.id})
+            .populate({
+                path: "comments",
+                select: "-__v"
+            })
+            .select("-__v")
             .then(dbPizzaData => {
                 if(!dbPizzaData) {
                     res.status(404).json({message: "No pizza found with this id."});
--- models/Pizza.js
+++ models/Pizza.js
@@ -1,21 +1,46 @@
+const moment = require("moment");
 const {Schema, model} = require("mongoose");
 
-const PizzaSchema = new Schema({
-    pizzaName: {
-        type: String
+const PizzaSchema = new Schema(
+    {
+        pizzaName: {
+            type: String
+        },
+        createdBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // Reformat date on backend using Moment.js
+            get: (createdAtVal) => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a")
+        },
+        size: {
+            type: String,
+            default: "Large"
+        },
+        toppings: [],
+        comments: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Comment"
+            }
+        ]
     },
-    createdBy: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    size: {
-        type: String,
-        default: "Large"
-    },
-    toppings: []
+    // Allows use of virtuals for comment count property.  This line not necessary if no virtuals
+    // Also allow the use of getters (allows Moment.js functionality)
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+// Get number of comments and replies on retrieval of a Pizza object
+PizzaSchema.virtual("commentCount").get(function() {
+    return this.comments.length;
 });
 
 const Pizza = model("Pizza", PizzaSchema);
